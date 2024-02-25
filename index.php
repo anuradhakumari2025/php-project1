d45ef6c175dad329c55deb9286ee04022c708212
anuradhakumari2025 <krianuradha4321@gmail.com>
2024-02-25 10:49:47 +0530

    Add Arithmetic Operators

--- index.php
+++ index.php
@@ -13,6 +13,29 @@
     echo $variable1;
     echo "<br>";
     echo $variable2;
+    echo "<br>";
+
+    //Operators in PHP
+    //Arithmetic Operators
+    echo "The value of variable 1 + variable 2 is : ";
+    echo $variable1 + $variable2;
+    echo "<br>";
+    echo "The value of variable 1 - variable 2 is : ";
+    echo $variable1 - $variable2;
+    echo "<br>";
+    echo "The value of variable 1 * variable 2 is : ";
+    echo $variable1 * $variable2;
+    echo "<br>";
+    echo "The value of variable 1 / variable 2 is : ";
+    echo $variable1 / $variable2;
+    echo "<br>";
+
+    //Assignment Operators
+    $newvar = $variable1;
+    echo $newvar;
+    echo "<br>";
+    $newvar += 1;
+    echo $newvar;
     ?>
 </body>
 </html>
