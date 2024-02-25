4c95029153b15ab50d2a83d120131c2c6a80cc04
anuradhakumari2025 <krianuradha4321@gmail.com>
2024-02-25 11:15:35 +0530

    Add Increment Operators

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     echo "Hello World <br>";
     $variable1 = 20;
     $variable2 = 21;
-    echo $variable1;
+    echo "The value of variable1 is : $variable1";
     echo "<br>";
-    echo $variable2;
+    echo "The value of variable2 is : $variable2";
     echo "<br>";
 
     //Operators in PHP
@@ -32,14 +32,16 @@
 
     //Assignment Operators
     $newvar1 = $variable1;
-    echo $newvar1;
+    echo "The value of newvar1 is : $newvar1" ;
     echo "<br>";
     $newvar1 += 1;
-    echo $newvar1;
+    echo "The value of newvar1 += 1 is : $newvar1";
     echo "<br>";
     $newvar2 = $variable2;
+    echo "The value of newvar2 is : $newvar2";
+    echo "<br>";
     $newvar2 *= 2;
-    echo $newvar2;
+    echo "The value of newvar2 *= 2 is : $newvar2";
     echo "<br>";
 
     //Comparison Operators
@@ -54,6 +56,20 @@
     echo "<br>";
     echo "The value of 1>=4 is ";
     echo var_dump(1>=4);
+    echo "<br>";
+
+    //Increment Operators//
+    echo "The value of variable1++ is :  "; 
+    echo $variable1++;
+    echo "<br>";
+    echo "The value of variable2++ is :  "; 
+    echo $variable2++;
+    echo "<br>";
+    echo "The value of ++newvar1 is :  "; 
+    echo ++$newvar1;
+    echo "<br>";
+    echo "The value of --newvar2 is :  "; 
+    echo --$newvar2;
 
     ?>
 </body>
