45c58b778903c02a10df568c4df3d1227b4fc9ac
anuradhakumari2025 <krianuradha4321@gmail.com>
2024-02-25 09:30:03 +0530

    Add index files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1</title>
+</head>
+<body>
+    <?php
+    echo "Hello World <br>";
+    $variable1 = 20;
+    $variable2 = 21;
+    echo $variable1;
+    echo "<br>";
+    echo $variable2;
+    ?>
+</body>
+</html>
+
+
